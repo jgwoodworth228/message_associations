680c5ffc62f49baa2ec565c52c4e03267d322d45
Arjun Venkataswamy <avenkat2@gmail.com>
2015-07-28 11:43:55 -0500

    adds comments to post show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   def user
     return User.find(user_id)
   end
+
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728013950) do
+ActiveRecord::Schema.define(version: 20150728163247) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "contents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
